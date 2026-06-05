dd76a4509450c91cf3e9af3e9953bb1141225459
Parham Hadavi <hadaviparham1@gmail.com>
2026-06-05 23:55:23 +0330

    Editing plant interface

--- core/models/entities/plants/IPlant.java
+++ core/models/entities/plants/IPlant.java
@@ -1,11 +1,12 @@
 package models.entities.plants;
 
 public interface IPlant {
-    getName();
-    getCategory();
-    getTags();
-    getCost();
-    getBaseHp();
-    getDamage();
-    getBaseAbility();
+    int getId();
+    String getName();
+    String getCategory();
+    String[] getTags();
+    int getCost();
+    int getBaseHp();
+    int getDamage();
+    String getBaseAbility();
 }
