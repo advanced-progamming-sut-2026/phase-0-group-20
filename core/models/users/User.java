80daf381b8158aed604e15b49415edb8570273bf
nikluas <elyas.hajinezhad86@gmail.com>
2026-06-09 21:09:04 +0330

    Implement User

--- core/models/users/User.java
+++ core/models/users/User.java
@@ -1,4 +1,134 @@
 package models.users;
 
+import models.enums.Gender;
+import models.enums.plants.SecurityQuestion;
+
 public class User {
+    private String username;
+    private String passwordHash;
+    private String nickname;
+    private String email;
+    private Gender gender;
+    private SecurityQuestion securityQuestion;
+    private String securityAnswerHash;
+    private int coin;
+    private int diamond;
+    private int gamesPlayed;
+    private int levelsCompleted;
+    private boolean stayLoggedIn;
+
+    public User(String username, String passwordHash,
+                String nickname, String email, Gender gender,
+                SecurityQuestion securityQuestion, String securityAnswerHash) {
+
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.nickname = nickname;
+        this.email = email;
+        this.gender = gender;
+        this.securityQuestion = securityQuestion;
+        this.securityAnswerHash = securityAnswerHash;
+        this.coin = 0;
+        this.diamond = 0;
+        this.gamesPlayed = 0;
+        this.levelsCompleted = 0;
+        this.stayLoggedIn = false;
+    }
+
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public SecurityQuestion getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(SecurityQuestion securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getSecurityAnswerHash() {
+        return securityAnswerHash;
+    }
+
+    public void setSecurityAnswerHash(String securityAnswerHash) {
+        this.securityAnswerHash = securityAnswerHash;
+    }
+
+    public int getCoin() {
+        return coin;
+    }
+
+    public void setCoin(int coin) {
+        this.coin = coin;
+    }
+
+    public int getDiamond() {
+        return diamond;
+    }
+
+    public void setDiamond(int diamond) {
+        this.diamond = diamond;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(int gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    public int getLevelsCompleted() {
+        return levelsCompleted;
+    }
+
+    public void setLevelsCompleted(int levelsCompleted) {
+        this.levelsCompleted = levelsCompleted;
+    }
+
+    public boolean isStayLoggedIn() {
+        return stayLoggedIn;
+    }
+
+    public void setStayLoggedIn(boolean stayLoggedIn) {
+        this.stayLoggedIn = stayLoggedIn;
+    }
 }
