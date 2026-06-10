1d1a832f1a1189ec881e63fe523e36a5a21e0f07
nikluas <elyas.hajinezhad86@gmail.com>
2026-06-10 17:14:06 +0330

    add some plant fodd strategy

---
+++ core/models/entities/plants/PlantFoodStrategy/SunProducingStrategy.java
@@ -0,0 +1,10 @@
+package models.entities.plants.PlantFoodStrategy;
+
+import models.entities.plants.Plant;
+
+public class SunProducingStrategy implements PlantFoodStrategy {
+    @Override
+    public void executeStrategy(Plant plant) {
+
+    }
+}
