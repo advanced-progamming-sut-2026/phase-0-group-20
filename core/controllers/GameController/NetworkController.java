8a4c5a9b1477463478c0dd2754671fe733b2b9cc
Hosein <hos3in.base@gmail.com>
2026-06-10 23:30:26 +0330

    Added MiniGames and Changed WinConditions and Added MiniGames entities

--- core/controllers/GameController/NetworkController.java
+++ core/controllers/GameController/NetworkController.java
@@ -2,7 +2,7 @@ package controllers.GameController;
 
 import models.Result;
 
-public class NewWorkController {
+public class NetworkController {
 
     public Result changeMenu(String newMenu) {
         return null;
