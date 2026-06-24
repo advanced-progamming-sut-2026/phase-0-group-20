1ef531376f9560290d7754d9885c4fab7920be62
Hosein <hos3in.base@gmail.com>
2026-06-25 00:22:46 +0330

    implemented the Mechanism of upgrading plants and collection menu

--- core/controllers/GameController/CollectionController.java
+++ core/controllers/GameController/CollectionController.java
@@ -1,43 +1,183 @@
 package controllers.GameController;
 
+import models.App;
 import models.Result;
+import models.entities.plants.Plant;
+import models.entities.zombies.Zombie;
+import models.enums.plants.PlantTag;
+import models.users.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class CollectionController {
-
+    private static final int MAX_LEVEL = 3;
+    private static final int BASE_COST = 200;
+    private static final int BASE_SEED_PACKETS =1;
+    private static final int PURCHASE_COST = 2000;
     public Result showPlants() {
-        return null;
+        User activeUser = App.getActiveUser();
+        ArrayList<Plant> unlockedPlants = activeUser.getUnlockedPlants();
+        StringBuilder result = new StringBuilder();
+        for (Plant plant : unlockedPlants) {
+            result.append(getPlantInfo(plant));
+        }
+        result.deleteCharAt(result.length() - 1);
+        return new Result(true, result.toString());
     }
 
     public Result showAllPlants() {
-        return null;
+
+        ArrayList<Plant> plants = App.getAllPlants();
+        StringBuilder result = new StringBuilder();
+        for (Plant plant : plants) {
+            result.append(getPlantInfo(plant));
+        }
+        result.deleteCharAt(result.length() - 1);
+        return new Result(true, result.toString());
     }
 
     public Result showZombies() {
-        return null;
+        User activeUser = App.getActiveUser();
+        ArrayList <Zombie> zombies = activeUser.getUnlockedZombies();
+        StringBuilder result = new StringBuilder();
+        for(Zombie zombie : zombies){
+            result.append(getZombieInfo(zombie));
+        }
+        result.deleteCharAt(result.length() - 1);
+        return new Result(true, result.toString());
     }
 
     public Result showAllZombies() {
-        return null;
+        ArrayList<Zombie> zombies = App.getAllZombies();
+        StringBuilder result = new StringBuilder();
+        for(Zombie zombie : zombies){
+            result.append(getZombieInfo(zombie));
+        }
+        result.deleteCharAt(result.length() - 1);
+        return new Result(true, result.toString());
+
     }
 
     public Result showPlantInfo(String name) {
-        return null;
+        ArrayList<Plant> plants = App.getAllPlants();
+        Plant foundPlant = plants.stream()
+                .filter(plant -> plant.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+        if(foundPlant == null){
+            return new Result(false, "Your desired plant doesn't exist.");
+        }
+        String rawText = getPlantInfo(foundPlant);
+        String text = rawText.substring(0, rawText.length() - 1);
+        return new Result(true,text);
     }
 
     public Result showZombieInfo(String name) {
-        return null;
+        ArrayList<Zombie> zombies = App.getAllZombies();
+        Zombie foundZombie = zombies.stream()
+                .filter(zombie->zombie.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+        if(foundZombie == null){
+            return new Result(false, "Your desired zombie doesn't exist.");
+        }
+        String rawText = getZombieInfo(foundZombie);
+        String text = rawText.substring(0, rawText.length() - 1);
+        return new Result(true,text);
     }
 
     public Result upgradePlant(String name) {
-        return null;
+        User activeUser = App.getActiveUser();
+        ArrayList<Plant> plants = activeUser.getUnlockedPlants();
+        Plant foundPlant = plants.stream()
+                .filter(plant->plant.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+        if(foundPlant == null){
+            return new Result(false, "Your desired plant doesn't exist.");
+        }
+        HashMap<String, Integer> seeds = activeUser.getInventory().getSeedPackets();
+        int cost = BASE_COST * foundPlant.getLevel();
+        int seedPacketCost = BASE_SEED_PACKETS * foundPlant.getLevel();
+        if(!seeds.containsKey(name)||seedPacketCost > seeds.get(name)){
+            return new Result(false, "You don't have enough seed packets to upgrade this plant.");
+        }
+        if(cost>activeUser.getCoin()){
+            return new Result(false, "You don't have enough coin to upgrade this plant.");
+        }
+        if(foundPlant.getLevel() == MAX_LEVEL){
+            return new Result (false, "The plant is already at max level.");
+        }
+        foundPlant.upgrade();
+        activeUser.costCoin(cost);
+        seeds.computeIfPresent("Sunflower", (k, v) -> Math.max(0, v - seedPacketCost));
+
+        return new Result(true, "You successfully upgraded "+foundPlant.getName()+" to level "+foundPlant.getLevel()+".");
     }
 
     public Result purchasePlant(String name) {
-        return null;
+        User activeUser = App.getActiveUser();
+        ArrayList<Plant> userPlants = activeUser.getUnlockedPlants();
+        ArrayList<Plant> plants = App.getAllPlants();
+        Plant foundPlant = plants.stream()
+                .filter(plant->plant.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+        if(foundPlant == null){
+            return new Result(false, "Your desired plant doesn't exist.");
+        }
+        if(userPlants.contains(foundPlant)){
+            return new Result(false,"You already have this plant.");
+        }
+        if(activeUser.getCoin()<BASE_COST){
+            return new Result(false,"You don't have enough coin to purchase this plant.");
+        }
+        activeUser.costCoin(PURCHASE_COST);
+        userPlants.add(foundPlant);
+        return new Result(true , "You successfully purchased "+foundPlant.getName()+" .");
+
     }
 
-    public Result changeMenu(String newMenu) {
-        return null;
+    private String formatTags(List<PlantTag> tags) {
+        if (tags.isEmpty()) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        for (PlantTag tag : tags) {
+            result.append(tag.name().toLowerCase()).append(", ");
+        }
+        result.delete(result.length() - 2, result.length());
+        return result.toString();
+    }
+
+    private String getPlantInfo(Plant plant) {
+        StringBuilder plantInfo = new StringBuilder();
+        plantInfo.append("-------------------------------\n");
+        String format = "%-15s : %s%n";
+        plantInfo.append(String.format(format, "Name", plant.getName()));
+        String formatedTags = formatTags(plant.getTags());
+        plantInfo.append(String.format(format, "Tags", formatedTags));
+        plantInfo.append(String.format(format, "Category", plant.getCategory().name().toLowerCase())); // not permanent
+        plantInfo.append(String.format(format, "Damage", plant.getDamage()));
+        plantInfo.append(String.format(format, "Base HP", plant.getBaseHp()));
+        plantInfo.append(String.format(format, "Base Ability", plant.getBaseAbility()));
+        plantInfo.append("-------------------------------\n\n");
+        return plantInfo.toString();
+    }
+
+    private String getZombieInfo(Zombie zombie){
+        StringBuilder result = new StringBuilder();
+        result.append("-------------------------------\n");
+        String format = "%-15s : %s%n";
+        result.append(String.format(format, "Name", zombie.getName()));
+        result.append(String.format(format, "Health", zombie.getBaseHp()));
+        result.append(String.format(format, "Speed",zombie.getSpeed()));
+        result.append("-------------------------------\n\n");
+        // not full
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
     }
 
 }
--- core/models/items/FoodPlant.java
+++ core/models/items/FoodPlant.java
@@ -1,4 +1,5 @@
 package models.items;
 
 public class FoodPlant implements Item {
+
 }
