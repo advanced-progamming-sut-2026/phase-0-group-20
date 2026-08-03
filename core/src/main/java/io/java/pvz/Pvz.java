675d0798ced5f1460aa447999b6520962eafc9e5
Hosein <hos3in.base@gmail.com>
2026-08-03 13:40:42 +0330

    initialized the base loader

--- core/src/main/java/io/java/pvz/Pvz.java
+++ core/src/main/java/io/java/pvz/Pvz.java
@@ -1,51 +1,47 @@
 package io.java.pvz;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import io.java.pvz.loader.AssetLoader;
 
-import pvz.libpvz.pam.PamPlayer;
-import pvz.libpvz.textures.TextureBank;
-
-public class Pvz extends ApplicationAdapter {
+public class Pvz extends Game {
     private SpriteBatch batch;
-
-    private TextureBank textures;
-    private PamPlayer player;
     private float stateTime;
 
-    private static final String ZOMBIE_PAM = "768/INITIAL/ZOMBIE/ZOMBIE_TUTORIAL/ZOMBIE_TUTORIAL.PAM";
+    private static final String ZOMBIE_PAM = "768/INITIAL/ZOMBIE/EGYPT_GARGANTUAR/EGYPT_GARGANTUAR.PAM";
     private static final String CLIP_NAME = "walk";
 
     @Override
     public void create() {
         batch = new SpriteBatch();
 
-        FileHandle assetsFolder = Gdx.files.internal("pvz2assets");
+        AssetLoader.getInstance().init();
 
-        textures = new TextureBank("768", assetsFolder);
-        player = new PamPlayer(textures, assetsFolder);
-
-        player.loadSync(ZOMBIE_PAM);
+        AssetLoader.getInstance().loadPamSync(ZOMBIE_PAM);
     }
 
     @Override
     public void render() {
         stateTime += Gdx.graphics.getDeltaTime();
 
-        textures.update();
+        AssetLoader.getInstance().updateTextures();
 
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        player.draw(batch, ZOMBIE_PAM, CLIP_NAME, stateTime, 300, 200, true);
+
+        AssetLoader.getInstance().getPlayer().draw(
+            batch, ZOMBIE_PAM, CLIP_NAME, stateTime, 300, 200, true
+        );
+
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        textures.dispose();
+        // آزادسازی منابع لودر
+        AssetLoader.getInstance().dispose();
     }
 }
--- core/src/main/java/io/java/pvz/loader/AssetLoader.java
+++ core/src/main/java/io/java/pvz/loader/AssetLoader.java
@@ -1,4 +1,53 @@
 package io.java.pvz.loader;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import pvz.libpvz.pam.PamPlayer;
+import pvz.libpvz.textures.TextureBank;
+
 public class AssetLoader {
+    private static AssetLoader instance;
+    private TextureBank textures;
+    private PamPlayer player;
+    public static AssetLoader getInstance ()
+    {
+        if (instance == null)
+        {
+            instance = new AssetLoader ();
+        }
+        return instance;
+    }
+    public void init(){
+        if (textures == null && player == null) {
+            FileHandle assetsFolder = Gdx.files.internal("pvz2assets");
+            textures = new TextureBank("768", assetsFolder);
+            player = new PamPlayer(textures, assetsFolder);
+        }
+    }
+
+    //load animation
+    public void loadPamSync(String pamPath) {
+        player.loadSync(pamPath);
+    }
+
+    // update animation
+    public void updateTextures() {
+        if (textures != null) {
+            textures.update();
+        }
+    }
+
+    public PamPlayer getPlayer() {
+        return player;
+    }
+
+    public void dispose() {
+        if (textures != null) {
+            textures.dispose();
+        }
+    }
+
+
+
+
 }
