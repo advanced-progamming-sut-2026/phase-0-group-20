9cd43263de20743f1de4a99fa4d659f68a5eb057
Elyas Hajinezhad <elyas.hajinezhad86@gmail.com>
2026-08-12 15:04:19 +0330

    ''

--- core/src/main/java/io/java/pvz/models/InGameEntityGenerator.java
+++ core/src/main/java/io/java/pvz/models/InGameEntityGenerator.java
@@ -123,10 +123,9 @@ public class InGameEntityGenerator {
     private static List<ZombieType> getDarkAgesZombies(int levelIndex) {
         return switch (levelIndex) {
             case 0 -> Arrays.asList(
-//                    ZombieType.NORMAL,
-//                    ZombieType.CONE,
-//                    ZombieType.JUGGLER
-                ZombieType.DARK_ARMOR
+                    ZombieType.NORMAL,
+                    ZombieType.CONE,
+                    ZombieType.JUGGLER
             );
             case 1 -> Arrays.asList(
                     ZombieType.NORMAL,
