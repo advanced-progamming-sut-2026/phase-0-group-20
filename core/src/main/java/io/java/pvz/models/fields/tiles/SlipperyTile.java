3ed65a14e67dc61c94008e5a24d003c19e5bd2ba
Parham Hadavi <hadaviparham1@gmail.com>
2026-08-15 15:17:24 +0330

    make zombie slide smoother

--- core/src/main/java/io/java/pvz/models/fields/tiles/SlipperyTile.java
+++ core/src/main/java/io/java/pvz/models/fields/tiles/SlipperyTile.java
@@ -2,6 +2,10 @@ package io.java.pvz.models.fields.tiles;
 
 import io.java.pvz.models.entities.plants.Plant;
 import io.java.pvz.models.entities.zombies.Zombie;
+import io.java.pvz.models.entities.zombies.behavior.effect.SlideCooldownEffect;
+import io.java.pvz.models.entities.zombies.behavior.effect.SlideEffect;
+import io.java.pvz.models.entities.zombies.behavior.effect.ZombieEffect;
+import io.java.pvz.models.enums.PhysicalConstants;
 import io.java.pvz.models.game.GameSession;
 
 import java.util.List;
@@ -24,10 +28,25 @@ public class SlipperyTile extends Tile {
         List<Zombie> zombiesOnTile = session.getArena().getZombiesOnTile(this);
 
 
-        for (Zombie zombie : zombiesOnTile)
+        for (Zombie zombie : zombiesOnTile) {
             if (zombie != null && !zombie.isDead()) {
-                slide(zombie);
+                boolean canSlide = true;
+                for (ZombieEffect e : zombie.getActiveEffects()) {
+                    if (e instanceof SlideEffect || e instanceof SlideCooldownEffect) {
+                        canSlide = false;
+                        break;
+                    }
+                }
+
+                float tileCenterX = (position.getCol() * PhysicalConstants.TILE_WIDTH) +
+                    PhysicalConstants.GRID_START_X + (PhysicalConstants.TILE_WIDTH / 2f);
+                float dx = Math.abs(zombie.getX() - tileCenterX);
+
+                if (canSlide && dx < 30f)
+                    slide(zombie);
+
             }
+        }
     }
 
     public void slide(Zombie zombie) {
@@ -36,7 +55,8 @@ public class SlipperyTile extends Tile {
         int targetRow = (direction == SlideDirection.UP) ? position.getRow() - 1 : position.getRow() + 1;
         if (targetRow < 0 || targetRow >= GameSession.getInstance().getArena().getRows()) return;
 
-        zombie.setRow(targetRow);
+        zombie.addEffect(new SlideEffect(zombie, targetRow));
+        GameSession.notify("Zombie is sliding " + direction + " to row " + (targetRow + 1));
     }
 
     public SlideDirection getDirection() {
