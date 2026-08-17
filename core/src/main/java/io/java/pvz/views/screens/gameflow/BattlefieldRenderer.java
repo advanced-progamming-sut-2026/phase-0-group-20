c1ca62ee2bc0fe99c8fc92a945d12c22ce52b5bb
Hosein <hos3in.base@gmail.com>
2026-08-17 18:21:02 +0330

    Added Freezing wind

--- core/src/main/java/io/java/pvz/views/screens/gameflow/BattlefieldRenderer.java
+++ core/src/main/java/io/java/pvz/views/screens/gameflow/BattlefieldRenderer.java
@@ -100,7 +100,10 @@ public class BattlefieldRenderer implements GameEventListener {
                 effectRenderer.spawnPianoBreakEffect(payload.getPixelX(), payload.getPixelY());
             } else if ("FREEZING_WIND".equals(payload.getMessage())) {
                 effectRenderer.spawnWindEffect(payload.getRow());
-            } else if ("SANDSTORM_START".equals(payload.getMessage())) {
+            }else if ("BOSS_WIND".equals(payload.getMessage())){
+                effectRenderer.spawnWindEffect(payload.getRow());
+                effectRenderer.spawnWindEffect(payload.getCol());
+            }else if ("SANDSTORM_START".equals(payload.getMessage())) {
                 effectRenderer.spawnSandstormEffect(payload.getZombie());
             } else if ("TARGET_LOCKED".equals(payload.getMessage())) {
                 effectRenderer.spawnTargetMarker(payload.getCol(), payload.getRow());
