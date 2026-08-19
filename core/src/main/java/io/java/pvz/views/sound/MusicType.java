6100e4b528d3a323bc29936b7d72611c3feede2b
Elyas Hajinezhad <elyas.hajinezhad86@gmail.com>
2026-08-19 09:16:23 +0330

    resolve music path

--- core/src/main/java/io/java/pvz/views/sound/MusicType.java
+++ core/src/main/java/io/java/pvz/views/sound/MusicType.java
@@ -2,28 +2,14 @@ package io.java.pvz.views.sound;
 
 public enum MusicType implements SoundType {
     MENU_BGM("sounds/bgm/1-01. Title Screen.mp3"),
-    ANCIENT_EGYPT("sounds/bgm/Ancient Egypt (First Wave) MP3" +
-        " - Plants vs. Zombies 2- It's About Time Original Soundtrack (Android, iOS) (gamerip) (2013) " +
-        "- Download Soundtracks for FREE!.mp3"),
-    BIG_WAVE_BEACH("sounds/bgm/Big Wave Beach (First Wave) MP3 " +
-        "- Plants vs. Zombies 2- It's About Time Original Soundtrack (Android, iOS) (gamerip) (2013) " +
-        "- Download Soundtracks for FREE!.mp3"),
-    FROZEN_CAVES("sounds/bgm/Frostbite Caves (Final Wave) MP3 " +
-        "- Plants vs. Zombies 2- It's About Time Original Soundtrack (Android, iOS) (gamerip) (2013) " +
-        "- Download Soundtracks for FREE!.mp3"),
-    DARK_AGES("sounds/bgm/Dark Ages (Final Wave) MP3 - Plants vs. Zombies 2" +
-        "- It's About Time Original Soundtrack (Android, iOS) (gamerip) (2013) " +
-        "- Download Soundtracks for FREE!.mp3")
+    ANCIENT_EGYPT("sounds/bgm/Ancient_Egypt_First_Wave_MP3_Plants_vs_Zombies_2_It's_About_Time.mp3"),
+    BIG_WAVE_BEACH("sounds/bgm/Big_Wave_Beach_First_Wave_MP3_Plants_vs_Zombies_2_It's_About_Time.mp3"),
+    FROZEN_CAVES("sounds/bgm/Frostbite_Caves_Final_Wave_MP3_Plants_vs_Zombies_2_It's_About_Time.mp3"),
+    DARK_AGES("sounds/bgm/Dark_Ages_Final_Wave_MP3_Plants_vs_Zombies_2_It's_About_Time_Original.mp3")
     ,ZEN_GARDEN("sounds/bgm/zen garden.mp3"),
-    ZOMBOSS_1("sounds/bgm/Zomboss (Phase 1) MP3 " +
-        "- Plants vs. Zombies 2- It's About Time Original Soundtrack (Android, iOS) (gamerip) (2013) " +
-        "- Download Soundtracks for FREE!.mp3"),
-    ZOMBOSS_2("sounds/bgm/Zomboss (Phase 2) MP3 - Plants vs. Zombies 2" +
-        "- It's About Time Original Soundtrack (Android, iOS) (gamerip) (2013)" +
-        " - Download Soundtracks for FREE!.mp3"),
-    ZOMBOSS_3("sounds/bgm/Zomboss (Phase 3) MP3 - Plants vs. Zombies 2" +
-        "- It's About Time Original Soundtrack (Android, iOS) (gamerip) (2013)" +
-        " - Download Soundtracks for FREE!.mp3");
+    ZOMBOSS_1("sounds/bgm/Zomboss_Phase_1_MP3_Plants_vs_Zombies_2_It's_About_Time_Original.mp3"),
+    ZOMBOSS_2("sounds/bgm/Zomboss_Phase_2_MP3_Plants_vs_Zombies_2_It's_About_Time_Original.mp3"),
+    ZOMBOSS_3("sounds/bgm/Zomboss_Phase_3_MP3_Plants_vs_Zombies_2_It's_About_Time_Original.mp3");
 
     private final String path;
     MusicType(String path){
