fb77f200139cf8612b3c09bf088268f9aa4e8e47
Elyas Hajinezhad <elyas.hajinezhad86@gmail.com>
2026-08-22 12:33:28 +0330

    reformat collection

--- core/src/main/java/io/java/pvz/controllers/GameController/CollectionController.java
+++ core/src/main/java/io/java/pvz/controllers/GameController/CollectionController.java
@@ -20,82 +20,6 @@ public class CollectionController {
     private static final int BASE_SEED_PACKETS = 10;
     private static final int PURCHASE_COST = 2000;
 
-    public Result showPlants() {
-        User activeUser = App.getActiveUser();
-        ArrayList<Plant> unlockedPlants = activeUser.getUnlockedPlants();
-        StringBuilder result = new StringBuilder();
-        if (unlockedPlants.isEmpty()) {
-            result.append("You have no plants in this game!");
-            return new Result(false, result.toString());
-        }
-        for (Plant plant : unlockedPlants) {
-            result.append(getPlantInfo(plant));
-        }
-        result.deleteCharAt(result.length() - 1);
-        return new Result(true, result.toString());
-    }
-
-    public Result showAllPlants() {
-
-        ArrayList<Plant> plants = App.getAllPlants();
-        StringBuilder result = new StringBuilder();
-        for (Plant plant : plants) {
-            result.append(getPlantInfo(plant));
-        }
-        result.deleteCharAt(result.length() - 1);
-        return new Result(true, result.toString());
-    }
-
-    public Result showZombies() {
-        User activeUser = App.getActiveUser();
-        ArrayList<Zombie> zombies = activeUser.getUnlockedZombies();
-        StringBuilder result = new StringBuilder();
-        for (Zombie zombie : zombies) {
-            result.append(getZombieInfo(zombie));
-        }
-        result.deleteCharAt(result.length() - 1);
-        return new Result(true, result.toString());
-    }
-
-    public Result showAllZombies() {
-        ArrayList<Zombie> zombies = App.getAllZombies();
-        StringBuilder result = new StringBuilder();
-        for (Zombie zombie : zombies) {
-            result.append(getZombieInfo(zombie));
-        }
-        result.deleteCharAt(result.length() - 1);
-        return new Result(true, result.toString());
-
-    }
-
-    public Result showPlantInfo(String name) {
-        ArrayList<Plant> plants = App.getAllPlants();
-        Plant foundPlant = plants.stream()
-                .filter(plant -> plant.getName().equalsIgnoreCase(name))
-                .findFirst()
-                .orElse(null);
-        if (foundPlant == null) {
-            return new Result(false, "Your desired plant doesn't exist.");
-        }
-        String rawText = getPlantInfo(foundPlant);
-        String text = rawText.substring(0, rawText.length() - 1);
-        return new Result(true, text);
-    }
-
-    public Result showZombieInfo(String name) {
-        ArrayList<Zombie> zombies = App.getAllZombies();
-        Zombie foundZombie = zombies.stream()
-                .filter(zombie -> zombie.getName().equalsIgnoreCase(name))
-                .findFirst()
-                .orElse(null);
-        if (foundZombie == null) {
-            return new Result(false, "Your desired zombie doesn't exist.");
-        }
-        String rawText = getZombieInfo(foundZombie);
-        String text = rawText.substring(0, rawText.length() - 1);
-        return new Result(true, text);
-    }
-
     public Result upgradePlant(String name) {
         User activeUser = App.getActiveUser();
         ArrayList<Plant> plants = activeUser.getUnlockedPlants();
@@ -179,53 +103,10 @@ public class CollectionController {
 
     }
 
-    private String formatTags(List<PlantTag> tags) {
-        if (tags.isEmpty()) {
-            return "";
-        }
-        StringBuilder result = new StringBuilder();
-        for (PlantTag tag : tags) {
-            result.append(tag.name().toLowerCase()).append(", ");
-        }
-        result.delete(result.length() - 2, result.length());
-        return result.toString();
-    }
-
-    private String getPlantInfo(Plant plant) {
-        StringBuilder plantInfo = new StringBuilder();
-        plantInfo.append("-------------------------------\n");
-        String format = "%-15s : %s%n";
-        plantInfo.append(String.format(format, "Name", plant.getName()));
-        String formatedTags = formatTags(plant.getTags());
-        plantInfo.append(String.format(format, "Tags", formatedTags));
-        plantInfo.append(String.format(format, "Category", plant.getCategory().name().toLowerCase())); // not permanent
-        plantInfo.append(String.format(format, "Damage", plant.getDamage()));
-        plantInfo.append(String.format(format, "Base HP", plant.getBaseHp()));
-        plantInfo.append(String.format(format, "Base Ability", plant.getAbilityType()));
-        plantInfo.append("-------------------------------\n\n");
-        return plantInfo.toString();
-    }
-
-    private String getZombieInfo(Zombie zombie) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("-------------------------------\n");
-        String format = "%-15s : %s%n";
-        stringBuilder.append(String.format(format, "Name", zombie.getName()));
-        stringBuilder.append(String.format(format, "Health", zombie.getBaseHp()));
-        stringBuilder.append(String.format(format, "Speed", zombie.getBaseSpeed()));
-        stringBuilder.append("-------------------------------\n\n");
-        // not full
-        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-        return stringBuilder.toString();
-    }
-
     public int getMaxLevel() {
         return MAX_LEVEL;
     }
 
-    public int getBaseCost() {
-        return BASE_COST;
-    }
 }
 
 
